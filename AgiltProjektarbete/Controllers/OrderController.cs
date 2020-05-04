b1fd4951dcfea735c800becfbe2ec2abf266c924
Emil Martini <emil.martini@hotmail.com>
2020-05-04 13:55:05 +0200

    stash

--- AgiltProjektarbete/Controllers/OrderController.cs
+++ AgiltProjektarbete/Controllers/OrderController.cs
@@ -1,16 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AgiltProjektarbete
 {
     public class OrderController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string id)
         {
             return View();
+        }
+
+        public IActionResult ConfirmOrder(string id)
+        {
+            return View();
+        }
+
+        public IActionResult AddOrder(string id)
+        {
+            return RedirectToAction("Index");
         }
     }
 }
