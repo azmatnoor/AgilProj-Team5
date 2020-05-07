b27012a73efd1dbe338b76f4e2891e41f193161b
Emil Martini <emil.martini@hotmail.com>
2020-05-07 09:23:35 +0200

    asdjhka

---
+++ AgiltProjektarbete/Controllers/ProfileController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgiltProjektarbete.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly ApplicationContext context;
+        private readonly UserManager<User> userManager;
+        public ProfileController(ApplicationContext context, UserManager<User> userManager)
+        {
+            this.context = context;
+            this.userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View(userManager.GetUserAsync(User).Result);
+        }
+    }
+}
